8b46a0c7a52a43479849338530234f9914478263
SONTAX <nichegon@gmail.com>
2023-12-14 17:18:55 +0200

    спроба працювати з VS: Code

--- test.py
+++ test.py
@@ -2,9 +2,5 @@ from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout
 
 app = QApplication([])
 window = QWidget()
-editor = QTextEdit()
-vert = QVBoxLayout()
-vert.addWidget(editor)
-window.setLayout(vert)
 window.show()
 app.exec_()
